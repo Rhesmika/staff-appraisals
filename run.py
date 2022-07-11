e6db1fc3067a6cedcab03fd54fd3871a892ae275
Rhesmika <Rhesmika@gmail.com>
2022-07-11 18:30:53 +0000

    Add function to get users name and run function from main function

--- run.py
+++ run.py
@@ -16,4 +16,18 @@ SHEET = GSPREAD_CLIENT.open('staff_appraisals')
 questions = SHEET.worksheet('questions')
 data = questions.get_all_values()
 
-print(data)
+
+def get_employee_name():
+    """
+    Gets users name
+    """
+    first_name = input("what is your first name? \n")
+    last_name = input("what is your last name? \n")
+    print(first_name, last_name)
+    return first_name, last_name
+
+
+def main():
+    get_employee_name()
+
+main()
