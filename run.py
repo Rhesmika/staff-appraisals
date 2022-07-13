ca49588b7f91eebaa70701256b0710691abf04e2
Rhesmika <Rhesmika@gmail.com>
2022-07-13 20:45:43 +0000

    Add function to ask question four, validate answer. Add answer to update sheet function

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
@@ -15,6 +14,7 @@ SHEET = GSPREAD_CLIENT.open('staff_appraisals')
 
 QUESTIONS = SHEET.worksheet('questions')
 data = QUESTIONS.get_all_values()
+
 
 
 def get_employee_name():
@@ -30,8 +30,6 @@ def get_employee_name():
             break
     return first_name, last_name
 
-
-
 def validate_name(full_name):
     """
     Checks users entry contains letters only. Raises error if not.
@@ -45,8 +43,8 @@ def validate_name(full_name):
         except ValueError as e:
             print(f"Invalid entry.{e}Please try again.")
             return False
-
     return True
+
 
 
 def question_one():
@@ -59,9 +57,6 @@ def question_one():
         if validate_rating(qi_rating):
             break
     return (qi_rating, )
-
-
-    
 
 def validate_rating(qi_rating):
     """
@@ -85,6 +80,8 @@ def validate_rating(qi_rating):
         question_one()
     return True
 
+
+
 def question_two():
     """
     Gets users rating to question two from spreadsheet
@@ -95,8 +92,6 @@ def question_two():
         if validate_rating(qii_rating):
             break
     return (qii_rating, )
-
-
 
 def validate_rating(qii_rating):
     """
@@ -121,6 +116,7 @@ def validate_rating(qii_rating):
     return True
 
 
+
 def question_three():
     """
     Gets users rating to question three from spreadsheet
@@ -131,9 +127,6 @@ def question_three():
         if validate_rating(qiii_rating):
             break
     return (qiii_rating, )
-
-
-    
 
 def validate_rating(qiii_rating):
     """
@@ -159,16 +152,51 @@ def validate_rating(qiii_rating):
 
 
 
+def question_four():
+    """
+    Gets users rating to question four from spreadsheet
+    """
+    while True:
+        topic = QUESTIONS.cell(1,6).value
+        qiv_rating = input(f"I {topic}: \n")
+        if validate_rating(qiv_rating):
+            break
+    return (qiv_rating, )
+
+def validate_rating(qiv_rating):
+    """
+    Validates if question four answer is a number. Raises error if not. 
+    """
+    try:
+        if qiv_rating.isnumeric()==False:
+            raise ValueError(
+                f"{qiv_rating} is not a valid entry."
+            )
+        if int(qiv_rating) > 5:
+            raise ValueError(
+                f"{qiv_rating} is more than 5."
+            )
+        if int(qiv_rating) < 1:
+            raise ValueError(
+                f"{qiv_rating} is less than 1."
+            )
+    except ValueError as e:
+        print(f"{e}\nPlease enter a number between 1-5...")
+        question_four()
+    return True
 
 
 
 
-def update_answers(names, qi_answer, qii_answer, qiii_answer):
+
+
+
+def update_answers(names, qi_answer, qii_answer, qiii_answer, qiv_answer):
     """
     Updates worksheet with user answers
     """
     print(f"Updating...\n")
-    data = (*names, *qi_answer, *qii_answer, *qiii_answer)
+    data = (*names, *qi_answer, *qii_answer, *qiii_answer, *qiv_answer)
     QUESTIONS.append_row(data)
     print(f"worksheet updated successfully\n")
 
@@ -183,7 +211,8 @@ def main():
     qi_answer = question_one()
     qii_answer = question_two()
     qiii_answer = question_three()
-    update_answers(names, qi_answer, qii_answer, qiii_answer)
+    qiv_answer = question_four()
+    update_answers(names, qi_answer, qii_answer, qiii_answer, qiv_answer)
 
 
     
