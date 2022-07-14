7442c0d1d283c8a58cef0f536886b1216a10c1f4
Rhesmika <Rhesmika@gmail.com>
2022-07-14 19:53:14 +0000

    Redefine validation method to rid of repetative validation functions

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ def validate_name(full_name):
     """
     for i in full_name:
         try:
-            if i.isalpha()==False:
+            if i.isalpha() == False:
                 raise ValueError(
                     f"Your name must alphabetical.\n'{i}' contains invalid symbols \n"
                 )
@@ -52,34 +52,11 @@ def question_one():
     Gets users rating to question one from spreadsheet
     """
     while True:
-        topic = QUESTIONS.cell(1,3).value
-        qi_rating = input(f"I {topic}: \n")
-        if validate_rating(qi_rating):
-            break
+            topic = QUESTIONS.cell(1,3).value
+            qi_rating = input(f"I {topic}: \n")
+            if validate_rating(qi_rating):          
+                break        
     return (qi_rating, )
-
-def validate_rating(qi_rating):
-    """
-    Validates if question one answer is a number. Raises error if not. 
-    """
-    try:
-        if qi_rating.isnumeric()==False:
-            raise ValueError(
-                f"{qi_rating} is not a valid entry."
-            )
-        if int(qi_rating) > 5:
-            raise ValueError(
-                f"{qi_rating} is more than 5."
-            )
-        if int(qi_rating) < 1:
-            raise ValueError(
-                f"{qi_rating} is less than 1."
-            )
-    except ValueError as e:
-        print(f"{e}\nPlease enter a number between 1-5...")
-        question_one()
-    return True
-
 
 
 def question_two():
@@ -87,102 +64,37 @@ def question_two():
     Gets users rating to question two from spreadsheet
     """
     while True:
-        topic = QUESTIONS.cell(1,4).value
-        qii_rating = input(f"I {topic}: \n")
-        if validate_rating(qii_rating):
-            break
+            topic = QUESTIONS.cell(1,4).value
+            qii_rating = input(f"I {topic}: \n")
+            if validate_rating(qii_rating):          
+                break        
     return (qii_rating, )
 
-def validate_rating(qii_rating):
-    """
-    Validates if question two answer is a number. Raises error if not. 
-    """
-    try:
-        if qii_rating.isnumeric()==False:
-            raise ValueError(
-                f"{qii_rating} is not a valid entry."
-            )
-        if int(qii_rating) > 5:
-            raise ValueError(
-                f"{qii_rating} is more than 5."
-            )
-        if int(qii_rating) < 1:
-            raise ValueError(
-                f"{qii_rating} is less than 1."
-            )
-    except ValueError as e:
-        print(f"{e}\nPlease enter a number between 1-5...")
-        question_two()
-    return True
 
 
-
-def question_three():
+def validate_rating(values):
     """
-    Gets users rating to question three from spreadsheet
+    Validates if question answer is a number between 1 and 5. Raises error if not. 
     """
-    while True:
-        topic = QUESTIONS.cell(1,5).value
-        qiii_rating = input(f"I {topic}: \n")
-        if validate_rating(qiii_rating):
-            break
-    return (qiii_rating, )
-
-def validate_rating(qiii_rating):
-    """
-    Validates if question three answer is a number. Raises error if not. 
-    """
-    try:
-        if qiii_rating.isnumeric()==False:
-            raise ValueError(
-                f"{qiii_rating} is not a valid entry."
-            )
-        if int(qiii_rating) > 5:
-            raise ValueError(
-                f"{qiii_rating} is more than 5."
-            )
-        if int(qiii_rating) < 1:
-            raise ValueError(
-                f"{qiii_rating} is less than 1."
-            )
-    except ValueError as e:
-        print(f"{e}\nPlease enter a number between 1-5...")
-        question_three()
-    return True
-
-
-
-def question_four():
-    """
-    Gets users rating to question four from spreadsheet
-    """
-    while True:
-        topic = QUESTIONS.cell(1,6).value
-        qiv_rating = input(f"I {topic}: \n")
-        if validate_rating(qiv_rating):
-            break
-    return (qiv_rating, )
-
-def validate_rating(qiv_rating):
-    """
-    Validates if question four answer is a number. Raises error if not. 
-    """
-    try:
-        if qiv_rating.isnumeric()==False:
-            raise ValueError(
-                f"{qiv_rating} is not a valid entry."
-            )
-        if int(qiv_rating) > 5:
-            raise ValueError(
-                f"{qiv_rating} is more than 5."
-            )
-        if int(qiv_rating) < 1:
-            raise ValueError(
-                f"{qiv_rating} is less than 1."
-            )
-    except ValueError as e:
-        print(f"{e}\nPlease enter a number between 1-5...")
-        question_four()
+    for rating in values:
+        try:
+            if rating.isnumeric() == False:
+                raise ValueError(
+                    "You must enter a whole number"
+                )
+            else:
+                int_rating = int(rating)
+                if int_rating > 5:
+                    raise ValueError(
+                        f"{int_rating} is more than 5."
+                    )
+                if int_rating < 1:
+                    raise ValueError(
+                        f"{int_rating} is less than 1."
+                    )
+        except ValueError as e:
+            print(f"{e}\nPlease enter a number between 1-5...")
+            return False
     return True
 
 
@@ -190,13 +102,81 @@ def validate_rating(qiv_rating):
 
 
 
+# def question_three():
+#     """
+#     Gets users rating to question three from spreadsheet
+#     """
+#     while True:
+#         topic = QUESTIONS.cell(1,5).value
+#         qiii_rating = input(f"I {topic}: \n")
+#         if validate_rating(qiii_rating):
+#             break
+#     return (qiii_rating, )
 
-def update_answers(names, qi_answer, qii_answer, qiii_answer, qiv_answer):
+# def validate_rating(qiii_rating):
+#     """
+#     Validates if question three answer is a number. Raises error if not. 
+#     """
+#     try:
+#         if qiii_rating.isnumeric()==False:
+#             raise ValueError(
+#                 f"{qiii_rating} is not a valid entry."
+#             )
+#         if int(qiii_rating) > 5:
+#             raise ValueError(
+#                 f"{qiii_rating} is more than 5."
+#             )
+#         if int(qiii_rating) < 1:
+#             raise ValueError(
+#                 f"{qiii_rating} is less than 1."
+#             )
+#     except ValueError as e:
+#         print(f"{e}\nPlease enter a number between 1-5...")
+#     return True
+
+
+
+# def question_four():
+#     """
+#     Gets users rating to question four from spreadsheet
+#     """
+#     while True:
+#         topic = QUESTIONS.cell(1,6).value
+#         qiv_rating = input(f"I {topic}: \n")
+#         if validate_rating(qiv_rating):
+#             break
+#     return (qiv_rating, )
+
+# def validate_rating(qiv_rating):
+#     """
+#     Validates if question four answer is a number. Raises error if not. 
+#     """
+#     try:
+#         if qiv_rating.isnumeric()==False:
+#             raise ValueError(
+#                 f"{qiv_rating} is not a valid entry."
+#             )
+#         if int(qiv_rating) > 5:
+#             raise ValueError(
+#                 f"{qiv_rating} is more than 5."
+#             )
+#         if int(qiv_rating) < 1:
+#             raise ValueError(
+#                 f"{qiv_rating} is less than 1."
+#             )
+#     except ValueError as e:
+#         print(f"{e}\nPlease enter a number between 1-5...")
+#     return True
+
+
+
+
+def update_answers(names, qi_answer, qii_answer):
     """
     Updates worksheet with user answers
     """
     print(f"Updating...\n")
-    data = (*names, *qi_answer, *qii_answer, *qiii_answer, *qiv_answer)
+    data = (*names, *qi_answer, *qii_answer)
     QUESTIONS.append_row(data)
     print(f"worksheet updated successfully\n")
 
@@ -210,9 +190,8 @@ def main():
     names = get_employee_name()
     qi_answer = question_one()
     qii_answer = question_two()
-    qiii_answer = question_three()
-    qiv_answer = question_four()
-    update_answers(names, qi_answer, qii_answer, qiii_answer, qiv_answer)
+
+    update_answers(names, qi_answer, qii_answer)
 
 
     
